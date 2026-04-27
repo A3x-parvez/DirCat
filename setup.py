8317d614e3bdde780b45afe0633b7e3c5655fb3b
Parvez <rijwanoolkarim@gmail.com>
2026-04-27 23:50:51 +0530

    Fix template not working issue

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
+    package_data={
+        "dircat": ["templates/*.json"],  # 🔥 THIS IS THE FIX
+    },
     install_requires=[
         "rich"
     ],
